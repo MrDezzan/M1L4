3cd73902991906b4c1417242bb5bed2c2480bda6
MrDezzan <gromovakuuu@mail.ru>
2025-03-09 19:14:55 +0500

    pokemons v2

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import telebot 
+import telebot, random
 from config import token
 
 from logic import Pokemon
 
 bot = telebot.TeleBot(token) 
 
+exp = 0
+lvl = 1
 @bot.message_handler(commands=['go'])
 def go(message):
     if message.from_user.username not in Pokemon.pokemons.keys():
@@ -13,6 +15,31 @@ def go(message):
         bot.send_photo(message.chat.id, pokemon.show_img())
     else:
         bot.reply_to(message, "Ты уже создал себе покемона")
+
+@bot.message_handler(commands=['feed'])
+def feed(message):
+    if message.from_user.username in Pokemon.pokemons.keys():
+        bot.reply_to(message, f'''Ты покормил своего покемона😋
+Он получил 10 опыта⬆️ Кол-во опыта до следующего уровня: {30 * lvl - exp}''')
+        exp += 10
+        if exp >= 30 * lvl:
+            lvl += 1
+            bot.reply_to(message, f'''Твой покемон вырос! Теперь у него {lvl} уровень!😊''')
+            exp = 0
+            if lvl == 5:
+                bot.reply_to_to(message, f'Твой покемон стал редким! Теперь у него {lvl} уровень!😊')
+    else:
+        bot.reply_to(message, "Сначала создай себе покемона")
+
+@bot.message_handler(commands=['pokemon'])
+def pokemon(message):
+    if message.from_user.username in Pokemon.pokemons.keys():
+        pokemon = Pokemon(message.from_user.username)
+        bot.send_message(message.chat.id, pokemon.info())
+        bot.send_photo(message.chat.id, pokemon.show_img())
+        bot.send_message(message.chat.id, f"Кол-во опыта: {exp}, До след. уровня: {30 * lvl - exp}. Уровень: {lvl}")
+    else:
+        bot.reply_to(message, "Сначала создай себе покемона")
 
 
 bot.infinity_polling(none_stop=True)
