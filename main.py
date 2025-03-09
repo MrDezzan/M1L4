37701482dd470d3c4406382597fa384d933bbbb9
MrDezzan <gromovakuuu@mail.ru>
2025-03-09 19:41:30 +0500

    pokemons v2

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import telebot, random
+import telebot, time, random
 from config import token
 
 from logic import Pokemon
 
 bot = telebot.TeleBot(token) 
+feedcooldown = 300
+lastfeed ={}
 
-exp = 0
-lvl = 1
 @bot.message_handler(commands=['go'])
 def go(message):
     if message.from_user.username not in Pokemon.pokemons.keys():
@@ -18,17 +18,25 @@ def go(message):
 
 @bot.message_handler(commands=['feed'])
 def feed(message):
-    global exp, lvl
-    if message.from_user.username in Pokemon.pokemons.keys():
-        exp += 10
+    user_id = (message.from_user.id)
+    current_time = time.time()
+    if message.from_user.username in Pokemon.pokemons.keys():   
+        if user_id in lastfeed and current_time - lastfeed[user_id] < feedcooldown:
+            remain_time = int(FEED_COOLDOWN - (current_time - last_feed_time[user_id]))
+            bot.reply_to(message, f"❌ Подожди {remain_time // 60} мин. {remain_time % 60} сек. перед следующим кормлением!") 
+            return
+        pokemon = Pokemon(message.from_user.username)
+        lastfeed[user_id] = current_time
+        expgain = random.randint(5, 15)
+        pokemon["exp"] += expgain
         bot.reply_to(message, f'''Ты покормил своего покемона😋
-Он получил 10 опыта⬆️ Кол-во опыта до следующего уровня: {30 * lvl - exp}''')
-        if exp >= 30 * lvl:
-            lvl += 1
-            bot.reply_to(message, f'''Твой покемон вырос! Теперь у него {lvl} уровень!😊''')
-            exp = 0
-            if lvl == 5:
-                bot.reply_to_to(message, f'Твой покемон стал редким! Теперь у него {lvl} уровень!😊')
+Он получил 10 опыта⬆️ Кол-во опыта до следующего уровня: {30 * pokemon["lvl"] - pokemon["exp"]}''')
+        if pokemon["exp"] >= 30 * lvl:
+            pokemon["lvl"] += 1
+            bot.reply_to(message, f'''Твой покемон вырос! Теперь у него {pokemon["lvl"]} уровень!😊''')
+            pokemon["exp"] = 0
+            if pokemon["lvl"] == 5:
+                bot.reply_to_to(message, f'Твой покемон стал редким! Теперь у него {pokemon["lvl"]} уровень!😊')
     else:
         bot.reply_to(message, "Сначала создай себе покемона")
 
@@ -38,7 +46,7 @@ def pokemon(message):
         pokemon = Pokemon(message.from_user.username)
         bot.send_message(message.chat.id, pokemon.info())
         bot.send_photo(message.chat.id, pokemon.show_img())
-        bot.send_message(message.chat.id, f"Кол-во опыта: {exp}, До след. уровня: {30 * lvl - exp}. Уровень: {lvl}")
+        bot.send_message(message.chat.id, f"Кол-во опыта: {pokemon["exp"]}, До след. уровня: {30 * pokemon["lvl"] - pokemon["exp"]}. Уровень: {pokemon["lvl"]}")
     else:
         bot.reply_to(message, "Сначала создай себе покемона")
 
